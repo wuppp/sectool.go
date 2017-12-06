47ca0fca10ee4959272fa59ec3657c3087e8c7f5
c26root <c26@c26deMacBook-Pro.local>
2017-12-06 18:17:36 +0800

    update httpbanner useragent

--- httpbanner/httpbanner.go
+++ httpbanner/httpbanner.go
@@ -25,7 +25,8 @@ var (
 )
 
 var headers = map[string]string{
-	"X-Type": "Scan",
+	"X-Type":     "Scan",
+	"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
 }
 
 type HttpInfo struct {
